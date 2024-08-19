48fc4cc84b154563680d63d665f48fe74aeea9a5
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-18 21:50:06 -0700

    Add clean_phone_numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,6 +8,12 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_numbers(phone_numbers)
+  phone_number_digits = phone_numbers.gsub(/\D/, '')
+  phone_number_digits[0] = '' if phone_number_digits.length.eql?(11) && phone_number_digits[0].eql?('1')
+  phone_number_digits.length.eql?(10) ? phone_number_digits : nil
+end
+
 def legislators_by_zipcode(zip) # rubocop:disable Metrics/MethodLength
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -36,21 +42,25 @@ end
 puts 'EventManager initialized.'
 
 contents = CSV.open(
-  'event_attendees.csv',
+  'event_attendees_full.csv',
   headers: true,
   header_converters: :symbol
 )
 
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
-
 contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id, form_letter)
+  p "Name: #{row[:first_name]}, Phone Number: #{clean_phone_numbers(row[:homephone])}"
 end
+
+# template_letter = File.read('form_letter.erb')
+# erb_template = ERB.new template_letter
+#
+# contents.each do |row|
+#   id = row[0]
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislators_by_zipcode(zipcode)
+#
+#   form_letter = erb_template.result(binding)
+#
+#   save_thank_you_letter(id, form_letter)
+# end
